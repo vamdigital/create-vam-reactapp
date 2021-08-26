555b8357c9d292a5149a8a4f97e2c05f425440cc
VAM Digital <vamdigitalltd@gmail.com>
2021-08-26 15:46:47 +0100

    feat: adds covearge report and includes coverage folder to list of gitignore

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 describe("App", () => {
   it("should test App Component", () => {
     render(<App />);
-    const textEl = screen.getByText("Hello Vite + React!");
+    const textEl = screen.getByText(/Hello from Vam Digital/i);
     expect(textEl).toBeInTheDocument();
   });
 });
