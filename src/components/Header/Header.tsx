73dc3ad56502e8845580462590867d402f429413
VAM Digital <vamdigitalltd@gmail.com>
2021-08-27 11:03:27 +0100

    feat: adds styledcomponents

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,61 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
+const HeaderWrapper = styled.header`
+  display: flex;
+  width: 100%;
+  padding: 10px 20px;
+  background: #dede;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NavWrapper = styled.nav`
+  display: flex;
+  width: 100%;
+`;
+
+const NavList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+`;
+
+const NavListItem = styled.li`
+  display: flex;
+  width: 40%;
+  justify-content: space-between;
+`;
+
+const CounterWrapper = styled.div`
+  display: flex;
+  width: 60%;
+  justify-content: space-between;
+
+  span {
+    display: flex;
+    font-size: 14px;
+  }
+`;
 export const HeaderComponent = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
+    <HeaderWrapper>
+      <NavWrapper>
+        <NavList>
+          <NavListItem>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/counter">Counter</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          </NavListItem>
+        </NavList>
+      </NavWrapper>
+      <CounterWrapper>
+        <span>CTXCount: 0</span>
+        <span>RTKCount: 0</span>
+      </CounterWrapper>
+    </HeaderWrapper>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
 import "./index.css";
 
+const GlobalStyles = createGlobalStyle`
+	html {
+		overflow-y: scroll;
+		box-sizing: border-box;
+	}
+
+	*, *:before, *:after{
+		box-sizing: inherit;
+	}
+	html, body {
+		overflow: hidden;
+		height: 100%;
+	}
+	body {
+		margin: 0;
+		padding: 0;
+		font-size: 16px;
+		line-height: 1.5;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+
+
+
+`;
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <BrowserRouter>
       <App />
     </BrowserRouter>
